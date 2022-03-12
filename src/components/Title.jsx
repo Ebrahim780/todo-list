549afd40bef09954763c15c207d5d8c23ab3263e
Ebrahim780 <akbrly42@gmail.com>
2022-03-12 19:12:30 +0330

    remove theme context

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -8,35 +8,25 @@ const Title = () => {
 
   return (
     <div className='title'>
-        <button className='title__button btn-floating' onClick={() => dispatch({ type: 'SET_LANG' })}>
-          <img className='title__icon--lang' src={farsi ? EN : FA} alt="language" />
-        </button>
+      <button className='title__button btn-floating' onClick={() => dispatch({ type: 'SET_LANG' })}>
+        <img className='title__icon--lang' src={farsi ? EN : FA} alt="language" />
+      </button>
       {farsi ?
         <div>
           <h4 className='title__header'>فهرست کارهای من</h4>
           <p className='title__content'>
-            {todos.length ?
-              `شما ${todos.length} کار برای انجام دارید`
-              :
-              `شما کاری برای انجام ندارید :)`
-            }
+            {todos.length ? `شما ${todos.length} کار برای انجام دارید` : `شما کاری برای انجام ندارید :)`}
           </p>
         </div>
         :
         <div>
           <h4 className='title__header'>My To Do List</h4>
           <p className='title__content'>
-            {todos.length ?
-              `You have ${todos.length} works to do`
-              :
-              `You don't have any work to do :)`
-            }
+            {todos.length ? `You have ${todos.length} works to do` : `You don't have any work to do :)`}
           </p>
         </div>
       }
-      <button className='title__button btn black'>
-        {farsi ? 'تاریک' : 'dark'}
-      </button>
+      <div></div>
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import Form from '../components/Form';
 import Title from '../components/Title';
 import ToDo from '../contexts/ToDo';
-import Theme from '../contexts/Theme';
 import PriorityList from './PriorityList';
 
 const App = () => {
   return (
     <ToDo>
-      <Theme>
         <Title />
         <Form />
         <PriorityList/>
-      </Theme>
     </ToDo>
   )
 }

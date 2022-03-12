011ab070fbeb222b0e925f44208888e4f90f6531
Ebrahim780 <akbrly42@gmail.com>
2022-03-12 18:09:57 +0330

    update appearance of language button

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,21 +1,19 @@
 import React, { useContext } from 'react';
 import { ToDoContext } from '../contexts/ToDo';
+import EN from '../assets/icon/en.png';
+import FA from '../assets/icon/fa.png';
 
 const Title = () => {
   const { todos, farsi, dispatch } = useContext(ToDoContext);
 
   return (
     <div className='title'>
-      <button
-        className='title__button btn'
-        onClick={() => dispatch({type: 'SET_LANG'})}
-      >
-        {farsi ? 'EN' : 'فا'}
-      </button>
+        <button className='title__button btn-floating' onClick={() => dispatch({ type: 'SET_LANG' })}>
+          <img className='title__icon--lang' src={farsi ? EN : FA} alt="language" />
+        </button>
       {farsi ?
         <div>
-          <h4 className='title__header'
-          >فهرست کارهای من</h4>
+          <h4 className='title__header'>فهرست کارهای من</h4>
           <p className='title__content'>
             {todos.length ?
               `شما ${todos.length} کار برای انجام دارید`
@@ -36,9 +34,7 @@ const Title = () => {
           </p>
         </div>
       }
-      <button
-        className='title__button btn black'
-      >
+      <button className='title__button btn black'>
         {farsi ? 'تاریک' : 'dark'}
       </button>
     </div>

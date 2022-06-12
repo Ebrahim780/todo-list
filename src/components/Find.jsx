292b492b8a986b09e68f93b984385202b612d0c2
Ebrahim <akbrly42@gmail.com>
2022-06-12 19:44:54 +0430

    change texts in search feature

--- src/components/Find.jsx
+++ src/components/Find.jsx
@@ -4,12 +4,7 @@ import WithResult from '../hoc/WithResult';
 
 const Find = ({ isSerach }) => {
   const { farsi, todos } = useContext(ToDoContext)
-
   const [result, setResult] = useState([])
-
-  useEffect(() => {
-    console.log(result)
-  }, [result])
 
   const searchHandler = event => {
     let value = event.target.value.trim()
@@ -25,10 +20,14 @@ const Find = ({ isSerach }) => {
         <input type='text'
           className='searchbar__input'
           onChange={searchHandler}
-          placeholder={farsi ? 'جستجو کن...' : 'Search...'} />
+          placeholder={farsi ? 'دنبال چه کاری می گردی؟' : 'What Are you Looking for?'} />
       </div>
       <div className='result'>
-        {result.map(todo => <WithResult todo={todo} key={todo.id} priority={todo.priority} />)}
+        {result.length ?
+          result.map(todo => <WithResult todo={todo} key={todo.id} priority={todo.priority} />)
+          :
+          <p>{farsi ? 'موردی وجود ندارد' : 'There is not any thing'}</p>
+        }
       </div>
     </div>
   )

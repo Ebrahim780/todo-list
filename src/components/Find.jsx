f6155944fb0aee00bcf9047fcdfa2178864bf9d6
Ebrahim <akbrly42@gmail.com>
2022-06-14 10:55:42 +0430

    resolve production issue

--- src/components/Find.jsx
+++ src/components/Find.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { ToDoContext } from '../contexts/ToDoProvider';
 import WithResult from '../hoc/WithResult';
 

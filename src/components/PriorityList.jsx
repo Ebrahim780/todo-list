42062b6b640ba5a533b58603899f1bfb6fdd545f
Ebrahim <akbrly42@gmail.com>
2023-12-07 11:58:53 +0330

    use framer motion for todos animation

--- src/components/PriorityList.jsx
+++ src/components/PriorityList.jsx
@@ -1,4 +1,5 @@
 import { useStateContext } from 'contexts/ToDoProvider'
+import { motion, AnimatePresence } from 'framer-motion'
 import translate from 'translate/list'
 import Todo from './Todo'
 
@@ -16,15 +17,27 @@ const PriorityList = ({ priority }) => {
   return (
     filteredTodos.length !== 0 && (
       <div className='col s12 m6 l4'>
-        <div className={`card-panel ${colors[priority]}`}>
-          <h5 className='list__header'>{translate[lang][priority]}</h5>
-
-          {filteredTodos.map(todo => (
-            <div className='list__item' key={todo.id}>
-              <Todo todo={todo} />
-            </div>
-          ))}
-        </div>
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3, type: 'keyframes', bounce: 0.1 }}
+            className={`card-panel ${colors[priority]}`}>
+            <h5 className='list__header'>{translate[lang][priority]}</h5>
+            {filteredTodos.map(todo => (
+              <AnimatePresence>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, type: 'tween', bounce: 0.1 }}
+                  className='list__item'
+                  key={todo.id}>
+                  <Todo todo={todo} />
+                </motion.div>
+              </AnimatePresence>
+            ))}
+          </motion.div>
+        </AnimatePresence>
       </div>
     )
   )
